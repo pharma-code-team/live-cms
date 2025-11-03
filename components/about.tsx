8d44b51403a21091775e0a4eb2f7643d348956fc
manioube <manioube@gmail.com>
2025-11-03 20:48:06 +0100

    removed footer

--- components/about.tsx
+++ components/about.tsx
@@ -42,7 +42,7 @@ export function About({
           <DialogTitle>About Pages CMS</DialogTitle>
           <DialogDescription>Pages CMS is an Open Source Content Management System built for static websites (Jekyll, Next.js, VuePress, Hugo, etc). It allows you to edit your website&apos;s content directly on GitHub via a user-friendly interface.</DialogDescription>
         </DialogHeader>
-        <footer className="grid grid-flow-col justify-stretch text-sm gap-x-2">
+        {/*<footer className="grid grid-flow-col justify-stretch text-sm gap-x-2">
           <a className={cn(buttonVariants({ variant: "outline", size: "sm" }), "w-full")} href="https://pagescms.org" target="_blank">
             <Chrome className="h-4 w-4 shrink-0 mr-2"/>
             Website
@@ -55,7 +55,7 @@ export function About({
             <Github className="h-4 w-4 shrink-0 mr-2"/>
             GitHub
           </a>
-        </footer>
+        </footer>*/}
       </DialogContent>
     </Dialog>
   )
